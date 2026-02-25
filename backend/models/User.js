c39ee1e70a6c28fa620f98991e1ac335e06b27bc
Usaid-Khan <usaid1521@gmail.com>
2026-02-26 04:20:13 +0500

    Defined Database Schemas

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        profileImageUrl: {type: String, default: null},
+        role: {type: String, enum: ["ADMIN", "MEMBER"], default: "MEMBER"},
+        careerPreferences: {type: String, trim: true, maxlength: 300}
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("User", UserSchema);
